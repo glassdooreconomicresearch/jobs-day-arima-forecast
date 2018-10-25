6b3f241315f591b7d03efe3e2673a751c1b3cb23
Andrew Chamberlain <andrew.chamberlain@glassdoor.com>
2018-10-25 11:52:18 -0700

    October 2018 Update

--- arima_jobs_day.py
+++ arima_jobs_day.py
@@ -7,7 +7,7 @@
 # Glassdoor Economic Research 
 # Web: glassdoor.com/research
 # ORIGINAL: July 2018
-# LAST UPDATE: September 27, 2018
+# LAST UPDATE: October 25, 2018
 
 import pandas as pd
 from statsmodels.tsa.seasonal import seasonal_decompose
@@ -19,11 +19,11 @@ os.chdir('/Users/andrew.chamberlain/GitHub/jobs-day-arima-forecast')
 
 # UPDATE MONTHLY: Choose starting and ending dates for ARIMA model.
 start = '2010-01-01'
-end = '2018-08-01'
+end = '2018-09-01'
 
 # UPDATE MONTHLY: Choose starting and ending dates for forecast ahead period.
-start_f = '2018-09-01'
-end_f = '2019-08-01'
+start_f = '2018-010-01'
+end_f = '2019-09-01'
 
 # Load BLS payrolls data (NSA, SA, and implied SA factors).
 # Source: https://data.bls.gov/timeseries/CEU0000000001
