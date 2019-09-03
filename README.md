c62109eca6c3916d104205a4b7feb60e80190418
Daniel Zhao <6125210+dbzhao@users.noreply.github.com>
2019-09-03 05:47:52 -0700

    August 2019 Update
    Run for August. Add run instructions to README.

--- README.md
+++ README.md
@@ -4,8 +4,16 @@
 
 This script (and accompanying data file) produces auto-ARIMA forecasts of BLS NSA payrolls, as well as implied BLS seasonal adjustment factors. Combines into a 12-month-ahead forecast of monthly SA nonfarm payrolls.
 
+## Running Instructions
+
+1. Update the monthly start/end date. `start_f` should be the start of the forecast period (generally the current month). `end_f` must be 11 months after `start_f`.
+2. Make sure the working directory is pointing at the right folder.
+3. Update `data.csv` using the latest data from BLS. Make sure to extend the empty rows too.
 Source (NSA Payrolls): https://data.bls.gov/timeseries/CEU0000000001
 Source (SA Payrolls): https://data.bls.gov/timeseries/CES0000000001
+4. Run from command line: `python arima_jobs_day.py`
+
+## Installation Instructions
 
 <b>Note:</b> First time use may require installation of pyramid.arima and statsmodels Python packages. Mac Terminal commands:
 
--- arima_jobs_day.py
+++ arima_jobs_day.py
@@ -1,10 +1,10 @@
-# SIMPLE ARIMA FORECAST OF BLS NONFARM PAYROLLS. 
-# Note: Produces ARIMA forecasts of BLS NSA payrolls, as well as implied 
+# SIMPLE ARIMA FORECAST OF BLS NONFARM PAYROLLS.
+# Note: Produces ARIMA forecasts of BLS NSA payrolls, as well as implied
 # BLS seasonal adjustment factors. Combines into a 12-month-ahead forecast
 # of monthly SA nonfarm payrolls.
 #
 # Andrew Chamberlain, Ph.D.
-# Glassdoor Economic Research 
+# Glassdoor Economic Research
 # Web: glassdoor.com/research
 # ORIGINAL: July 2018
 # LAST UPDATE: June 28, 2019
@@ -15,15 +15,15 @@ from pyramid.arima import auto_arima
 import os
 
 # Set working directory (CHANGE THIS FOR YOUR ENVIRONMENT)
-os.chdir('/Users/andrew.chamberlain/GitHub/jobs-day-arima-forecast')
+os.chdir('/Users/daniel.zhao/Documents/GitHub/jobs-day-arima-forecast')
 
 # UPDATE MONTHLY: Choose starting and ending dates for ARIMA model.
 start = '2010-01-01'
-end = '2019-06-01'
+end = '2019-07-01'
 
 # UPDATE MONTHLY: Choose starting and ending dates for forecast ahead period.
-start_f = '2019-07-01'
-end_f = '2020-06-01'
+start_f = '2019-08-01'
+end_f = '2020-07-01'
 
 # Load BLS payrolls data (NSA, SA, and implied SA factors).
 # Source: https://data.bls.gov/timeseries/CEU0000000001
@@ -38,7 +38,7 @@ decomp = seasonal_decompose(df.loc[start:end,'payrolls_nsa'], model='additive', 
 fig = decomp.plot()
 fig.savefig('decomp.png')
 
-# Examine components of the NSA payrolls time series decomposition. 
+# Examine components of the NSA payrolls time series decomposition.
 trend = decomp.trend
 seasonal = decomp.seasonal
 resid = decomp.resid
@@ -52,8 +52,8 @@ model_nsa = auto_arima(df.loc[start:end,'payrolls_nsa'], exogenous=None, start_p
                            max_p=6, max_q=6, m=12,
                            start_P=0, seasonal=True,
                            d=1, D=1, trace=True,
-                           error_action='ignore',  
-                           suppress_warnings=True, 
+                           error_action='ignore',
+                           suppress_warnings=True,
                            stepwise=True)
 model_nsa.summary()
 
@@ -62,32 +62,34 @@ model_sa_factors = auto_arima(df.loc[start:end,'implied_sa_factors'], exogenous=
                            max_p=6, max_q=6, m=12,
                            start_P=0, seasonal=True,
                            d=1, D=1, trace=True,
-                           error_action='ignore',  
-                           suppress_warnings=True, 
+                           error_action='ignore',
+                           suppress_warnings=True,
                            stepwise=True)
 model_sa_factors.summary()
 
 
 #################################################
-# Create 12-month ahead forecast of NSA payrolls. 
+# Create 12-month ahead forecast of NSA payrolls.
 #################################################
 
 # Create training and forecast data frames.
 train = df.loc[start:end,'payrolls_nsa']
 test = df.loc[start_f:end_f, 'payrolls_nsa']
 
-# Fit arima model on NSA payrolls time series. 
+# Fit arima model on NSA payrolls time series.
 model_nsa.fit(train)
 
-# Forecast 12 months ahead. 
+# Forecast 12 months ahead.
 forecast_nsa = model_nsa.predict(n_periods = 12)
 
 # Create forecast dataframe.
-forecast_nsa_df = pd.DataFrame(forecast_nsa, index=test.index, columns=['prediction_nsa'])
+forecast_nsa_df = pd.DataFrame(forecast_nsa,
+                               index=test.index,
+                               columns=['prediction_nsa'])
 
 
 ###########################################################
-# Create 12-month ahead forecast of BLS implied SA factors. 
+# Create 12-month ahead forecast of BLS implied SA factors.
 ###########################################################
 
 # Create training and forecast data frames.
@@ -97,7 +99,7 @@ test_sa_factors = df.loc[start_f:end_f, 'implied_sa_factors']
 # Fit arima model on BLS implied SA factors time series.
 model_sa_factors.fit(train_sa_factors)
 
-# Forecast 12 months ahead. 
+# Forecast 12 months ahead.
 forecast_sa_factors = model_sa_factors.predict(n_periods = 12)
 
 # Create forecast dataframe.
@@ -108,10 +110,10 @@ forecast_sa_factors_df = pd.DataFrame(forecast_sa_factors, index=test.index, col
 # Merge full forecast data together.
 ###########################################
 
-# Merge together forecasts of NSA payrolls and implied BLS SA factors. 
+# Merge together forecasts of NSA payrolls and implied BLS SA factors.
 df_forecast = pd.concat([df,forecast_nsa_df,forecast_sa_factors_df],axis=1)
 
-# Generate new column for 12-month ahead forecast of SA payrolls (NSA forecast minus forecasted SA factors). 
+# Generate new column for 12-month ahead forecast of SA payrolls (NSA forecast minus forecasted SA factors).
 df_forecast['forecast_sa'] = df_forecast['prediction_nsa'] - df_forecast['prediction_sa_factors']
 
 # Push out 12-month SA nonfarm payrolls forecast to CSV.
